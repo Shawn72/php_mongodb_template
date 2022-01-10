fdc2a9463445db003098e260265d520e010b13f0
Shawn Ngutu <shawn.ngutu@smartapplicationsgroup.com>
2022-01-10 17:49:37 +0300

    php tests added

---
+++ tests/validatePasswordTest.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../src/validatePassword.php'); //dont forget this
+
+class ValidatePasswordTest extends PHPUnit_Framework_TestCase{
+
+    public function testValidLength(){
+        $valpass = new ValidatePassword();
+        $this->assertFalse($valpass-> validLength("1234")); //fails if the passlength is required (6)
+        $this->assertTrue($valpass-> validLength("123456")); //pass if the passlength is required (6)
+    }
+}
