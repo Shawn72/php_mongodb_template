a2385bed7272ec5c73c88523557a85b74c126234
Shawn Andre Mbuvi <shawnmbuvi@gmail.com>
2022-01-12 11:20:34 +0300

    Update validatePasswordTest.php

--- tests/validatePasswordTest.php
+++ tests/validatePasswordTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class ValidatePasswordTest extends TestCase{
 
     public function testValidLength(){
-        $valpass = new ValidatePassword();
+        $valpass = new validatePassword();
         $this->assertFalse($valpass-> validLength("1234")); //fails if the passlength is required (6)
         $this->assertTrue($valpass-> validLength("123456")); //pass if the passlength is required (6)
     }
